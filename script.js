0bf8bb32132559b695a57193cca044b6b5ac0445
Bipul Mondal <bipulmondal3007@gmail.com>
2022-12-28 13:57:23 +0530

    update all page

---
+++ script.js
@@ -0,0 +1,37 @@
+function goto1() {
+    window.location.href="./index.html";
+}
+function goto2() {
+    window.location.href = "./about.html";
+}
+function goto3() {
+    window.location.href="./work.html";
+}
+function goto4() {
+    window.location.href="./contact.html";
+}
+
+document.querySelector(".btn_1").addEventListener("click", work)
+    function work(){
+        window.location.href="./work.html";
+    }
+document.querySelector(".btn_2").addEventListener("click", hire)
+    function hire(){
+        window.location.href="./contact.html";
+    }
+document.querySelector("#img1").addEventListener("click", linkedin)
+    function linkedin(){
+
+    }
+document.querySelector("#img2").addEventListener("click", instagram)
+    function instagram(){
+        
+    }
+document.querySelector("#img3").addEventListener("click", gmail)
+    function gmail(){
+        
+    }
+document.querySelector("#img4").addEventListener("click", github)
+    function github(){
+        
+    }
